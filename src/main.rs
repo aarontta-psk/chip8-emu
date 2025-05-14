2ed0ce237a33f97d7b6927c88be4e0d37e3e05d4
aarontta-psk <207921825+aarontta-psk@users.noreply.github.com>
2025-05-14 13:01:04 +0200

    Chip8 interpreter instructions

--- src/main.rs
+++ src/main.rs
@@ -30,6 +30,10 @@ fn main() {
     let contents = fs::read("Airplane.ch8").expect("Should have been able to read the file");
     println!("{}\n{:#04x?}", msg, contents);
 
+    let file = fs::read_to_string("instr.txt").expect("Should have been able to read the file");
+    let instr: Vec<&str> = file.split("\r\n\r\n").collect();
+    println!("{:?}", instr);
+
     // let emu: Chip8Registry = Chip8Registry::new();
     // println!("{:?} {} {} {} {} {} {:?}", emu.vx, emu.i, emu.pc, emu.sp, emu.dt, emu.st, emu.stack);
 }
