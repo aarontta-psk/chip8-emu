6a2b7343511d41ba72a3da6856d1ad9978d329da
aarontta-psk <207921825+aarontta-psk@users.noreply.github.com>
2025-05-14 13:04:34 +0200

    "Attempt" at processing instructions
    This is VERY ugly, bad, and probably breaks 5k best practices.
    I'm doing this to further comprehend how instructions are organized on the ROM file.

--- src/main.rs
+++ src/main.rs
@@ -44,6 +44,90 @@ impl Chip8Registry {
     }
 }
 
+fn process_instr<'a>(command: &u16, instr: &'a Vec<&str>) -> &'a str {
+    let first_num: u8 = ((command & 0xF000) >> 12) as u8;
+    let second_num: u8 = ((command & 0xF00) >> 8) as u8;
+    let third_num: u8 = ((command & 0xF0) >> 4) as u8;
+    let fourth_num: u8 = (command & 0xF) as u8;
+
+    match first_num {
+        0x0 => match third_num {
+            0xC => return instr[1],
+            0xE => {
+                if fourth_num == 0x0 {
+                    return instr[2];
+                } else {
+                    return instr[3];
+                }
+            }
+            0xF => return instr[4],
+            _ => return instr[0],
+        },
+        0x1 => return instr[9],
+        0x2 => return instr[10],
+        0x3 => return instr[11],
+        0x4 => return instr[12],
+        0x5 => return instr[13],
+        0x6 => return instr[14],
+        0x7 => return instr[15],
+        0x8 => match fourth_num {
+            0x0 => return instr[16],
+            0x1 => return instr[17],
+            0x2 => return instr[18],
+            0x3 => return instr[19],
+            0x4 => return instr[20],
+            0x5 => return instr[21],
+            0x6 => return instr[22],
+            0x7 => return instr[23],
+            0xE => return instr[24],
+            _ => return "what",
+        },
+        0x9 => return instr[25],
+        0xA => return instr[26],
+        0xB => return instr[27],
+        0xC => return instr[28],
+        0xD => {
+            if fourth_num == 0x0 {
+                return instr[29];
+            } else {
+                return instr[30];
+            }
+        }
+        0xE => {
+            if third_num == 0x9 {
+                return instr[31];
+            } else {
+                return instr[32];
+            }
+        }
+        0xF => return instr[33],
+        _ => return "what",
+    }
+}
+
+fn process_input(contents: &Vec<u8>, instr: &Vec<&str>) -> () {
+    let mut command: u16 = 0;
+    let mut count: u16 = 0;
+    let mut flag: bool = true;
+
+    for elem in contents.iter() {
+        if flag {
+            command = (*elem as u16) << 8;
+        } else {
+            command += *elem as u16;
+            println!(
+                "Instruction {}: {:#06x} -> {}",
+                count / 2,
+                command,
+                process_instr(&command, instr)
+            );
+            command = 0;
+        }
+        count += 1;
+        flag = !flag;
+    }
+}
+
 fn main() {
     let msg: String = String::from("Chip 8 Emulator!");
     let contents = fs::read("Airplane.ch8").expect("Should have been able to read the file");
@@ -53,6 +137,8 @@ fn main() {
     let instr: Vec<&str> = file.split("\r\n\r\n").collect();
     println!("{:?}", instr);
 
+    process_input(&contents, &instr);
+   
     // let emu: Chip8Registry = Chip8Registry::new();
     // println!("{:?} {} {} {} {} {} {:?}", emu.vx, emu.i, emu.pc, emu.sp, emu.dt, emu.st, emu.stack);
 }
